5c6fd4485dba7302d5aee538a77168feca505017
Dominik Widomski <dominik@digital-detox.co.uk>
2018-04-19 09:11:56 +0100

    chaining - final cleanup

--- chaining/main.js
+++ chaining/main.js
@@ -19,49 +19,38 @@ function addOne(x) {
 function chain(fns) {
 	const handler = {
 		get(target, propKey, receiver) {
-			// console.log('----------------------------------------------------');
-			// console.log('Trying to call:', target, propKey);
-			// console.dir(receiver); // WTF!??!??!?
-
-			let { value } = target;
+			const { value } = target;
 
 			if (propKey === "execute") {
-				// console.log('Return value', value);
 				return () => value;
 			}
 
 			let originalMethod = fns[propKey];
 			if (!originalMethod) {
-				// console.log('method not found in passed functions');
+				return () => receiver;
 			}
 
-			// if not using `this` change to ()=>{}
-			return function (...args) {
+			return (...args) => {
 				if (value) {
 					args = [value, ...args];
 				}
 
-				value = originalMethod.apply(this, args);
-
-				// Would like to avoid creating new Proxy every time
-				// but not sure if viable since at each step we're creating
-				// a new branch in the chain effectively
-				return new Proxy({ value }, handler);
+				return new Proxy({
+					value: originalMethod.apply(this, args)
+				}, handler);
 			}
 		}
 	}
 
-	return new Proxy({ value: null }, handler);
+	return new Proxy({ value: undefined }, handler);
 }
 
 var c = chain({ sum, minus, double, addOne });
 
-const result = c.sum(4, 5).sum(5).minus(4).sum(7).addOne().double().double().execute(); // 72
+const result = c.sum(4, 5).invalidFunction().sum(5).minus(4).sum(7).addOne().double().double().execute(); // 72
 
-assert(result === 72);
-// console.log(result === 72 ? "PASSED" : `FAILED: ${JSON.stringify(result)}`);
+assert.equal(result, 72);
 
-//*
 var c1 = c.sum(1, 2);
 c1.execute(); // == fns.sum(1, 2) == 3
 c1.double().execute(); // == fns.double(fns.sum(1, 2)) == 6
@@ -71,28 +60,8 @@ c1.execute(); // == fns.sum(1, 2) == 3
 var c2 = c1.sum(5);
 c2.addOne().execute(); // == fns.addOne(fns.sum(fns.sum(1, 2) 5)) == 9
 c2.sum(3).execute(); // == fns.sum(c1.sum(fns.sum(1, 2), 5), 3) == 11
-const r2 = c2.execute(); // == fns.sum(fns.sum(1, 2), 5) == 8
 
-const r1 = c1.execute(); // == fns.sum(1, 2) == 3
-
-assert(r2 === 8);
-assert(r1 === 3);
-//*/
-
-// function traceMethodCalls(obj) {
-// 	let handler = {
-// 		get(target, propKey, receiver) {
-// 			const originalMethod = target[propKey];
-// 			return function(...args) {
-// 				let result = originalMethod.apply(this, args);
-// 				console.log(propKey + JSON.stringify(args) + " -> " + JSON.stringify(result));
-// return result;
-//             }
-//         }
-// 	};
-
-// 	return new Proxy(obj, handler);
-// }
+assert.equal(c2.execute(), 8); // == fns.sum(fns.sum(1, 2), 5) == 8
+assert.equal(c1.execute(), 3); // == fns.sum(1, 2) == 3
 
 console.log("All tests PASSED! 👍");
-
